137b3ee054c156bf43f35d243469c707843e4f66
viniciusdc <vinivdc2009@hotmail.com>
2020-06-10 03:47:43 -0300

    Using Logging (rm outdated approuch)

--- LOG.py
+++ LOG.py
@@ -1,9 +1,11 @@
 from Methods.utils import rmsd, mde
 from datetime import datetime
+import logging
+import json
 import sys
 
 
-def os_display_call(log, local_dir, main, data, multistart=False):
+def os_display_call(test_path, main, data, multistart=False):
     (
         filename,
         num_atom_init,
@@ -16,156 +18,141 @@ def os_display_call(log, local_dir, main, data, multistart=False):
         ops,
     ) = main
     xi, solution, u, v, lb, ub = ops
-
-    print(
-        "#####################################  INFO  #####################################"
+    # Get logger
+    logger = logging.getLogger('root.spgLOG')
+    logger.info(
+        "##########################################  INFO  ##########################################"
     )
-    print(
+    logger.info(
         f":: Protein: {filename}, Initial atoms number: {num_atom_init}, after re-ordination {total_atoms_ord}."
     )
-    print(f":: Assessed distances: {m} and known distances: {prop_dist}.")
+    logger.info(f":: Assessed distances: {m} and known distances: {prop_dist}.")
     if convex:
-        print(
+        logger.info(
             f":: Initial objective value for the relaxed problem: {fo_non_scaled:.4e}"
         )
-        print(
+        logger.info(
             f":: Initial objective value for the relaxed problem --scaled {fo_scaled:.4e}"
         )
     rmsd_i, mde_i = rmsd(xi, solution), mde(xi, u, v, lb, ub)
-    print(f":: RMSDi = {rmsd_i:<24.2e} MDEi = {mde_i:.2e}")
+    logger.info(f":: RMSDi = {rmsd_i:<24.2e} MDEi = {mde_i:.2e}")
 
     # -----------------------------------------------------------------------------------
     # Multi-start option --Enabled
     # -----------------------------------------------------------------------------------
     if multistart:
         if type(data) != dict:
-            print(":: data type object not match with dict structure!")
-            print(":: The process was interrupted")
+            logger.warning(":: data type object not match with dict structure!")
+            logger.warning(":: The process was interrupted")
             return exit()
-        print(":: spg results --multi start: True")
-        print(
-            ":: Iter -- bck -- RMSDf ----- MDEf"
+        logger.info(":: spg results --multi start: True")
+        logger.info(
+            ":: Iter - bck -- RMSDf ----- MDEf"
             " ----- i_val ----- f_val ----- gtd ----- |d| ----- time(s)"
         )
         sub_log = {}
+        k = 0
         for key in data:
             out, elapsed_time, fo = data[key]
-            x_spg, backtracking, iterations, fun_o, gtd, norm_d, log_spg = out
+            x_spg, backtracking, iterations, fun_o, gtd, norm_d = out
             # Statistics:
             rmsd_f = rmsd(x_spg, solution)
             mde_f = mde(x_spg, u, v, lb, ub)
             prompt_string = (
-                f"   {iterations:<7} {backtracking:<6} {rmsd_f:<11.2e} {mde_f:<10.2e} {fo / 2:<11.2e} "
+                f"   {iterations:<5}: {backtracking:<6} {rmsd_f:<11.2e} {mde_f:<10.2e} {fo / 2:<11.2e} "
                 f"{fun_o / 2:<10.2e} {gtd:<10.2e} {norm_d:<10.2e} {elapsed_time:.3f}"
             )
-            sub_log[key] = prompt_string
-            print(prompt_string)
-        print(
-            "##################################################################################"
+            sub_log[k] = {"iter": f'{iterations:<7}', "back": f'{backtracking:<6}', "RMDSf": f'{rmsd_f:<11.2e}',
+                          "MDEf": f'{mde_f:<10.2e}', "fun_i": f'{fo / 2:<11.2e}', "fun_f": f'{fun_o / 2:<10.2e}',
+                          "gtd": f'{gtd:<10.2e}', "norm_d": f'{norm_d:<10.2e}', "time": f'{elapsed_time:.3f}'}
+            logger.info(prompt_string)
+            k += 1
+
+        logger.info(
+            "############################################################################################"
         )
 
         # -----------------------------------------------------------------------------
         # Generating output file with statistics:
         # -----------------------------------------------------------------------------
-        now = datetime.now()
-        dt_time = now.strftime("%d-%m-%Y--%H-%M-%S")
-        out_file_log = local_dir + f"\\Teste {filename}\\LOG_({dt_time}).txt"
-        with open(out_file_log, "w") as f:
-            for item in log:
-                if type(item) == list:
-                    for item_spg in item:
-                        f.write(f"{item_spg}\n")
-                    continue
-                f.write(f"{item}\n")
-            f.write(
-                "#####################################  INFO  #####################################\n"
-            )
-            f.write(
-                f":: Protein: {filename}, Initial atoms number: {num_atom_init}, "
-                f"after re-ordination {total_atoms_ord}.\n"
-            )
-            f.write(f":: Assessed distances: {m} and known distances: {prop_dist}.\n")
-            if convex:
-                f.write(
-                    f":: Initial objective value for the relaxed problem: {fo_non_scaled:.4e}\n"
-                )
-                f.write(
-                    f":: Initial objective value for the relaxed problem --scaled {fo_scaled:.4e}\n"
-                )
-            f.write(f":: RMSDi = {rmsd_i:<24.2e} MDEi = {mde_i:.2e}\n")
-            if type(data) != dict:
-                print(":: data type object not match with dict structure!\n")
-                print(":: The process was interrupted\n")
-            f.write(":: spg results --multi start: True\n")
-            f.write(
-                ":: Iter -- bck -- RMSDf ----- MDEf ----- i_val ----- f_val ----- gtd ----- |d| ----- time(s)\n"
-            )
-            for key in sub_log:
-                f.write(f"{sub_log[key]}\n")
-            f.write(
-                "##################################################################################"
-            )
+        static_dict = {"node": f'{filename}', "init_atom_#": f"{num_atom_init}",
+                       "atom_#_re-ordination": f'{total_atoms_ord}',
+                       "assessed_dist": f'{m}', "Know_dist": f'{prop_dist}'}
+        if convex:
+            static_dict["convex"] = True
+            static_dict["init_fun_val_relax"] = f'{fo_non_scaled:.4e}'
+            static_dict["init_fun_val_relax_k"] = f'{fo_scaled:.4e}'
+        else:
+            static_dict["convex"] = False
+            static_dict["init_fun_val_relax"] = 'N/A'
+            static_dict["init_fun_val_relax_k"] = 'N/A'
+
+        static_dict["RMSDi"] = f'{rmsd_i:<24.2e}'
+        static_dict["MDEi"] = f'{mde_i:.2e}'
+        if type(data) != dict:
+            logger.warning(":: data type object not match with dict structure!\n")
+            logger.warning(":: The process was interrupted\n")
+        multistart_list = []
+        n = len(sub_log.keys())
+        for i in range(n):
+            multistart_list.append(sub_log[i])
+        static_dict["multi-start"] = multistart_list
+        static_dict["standard"] = False
+
+        static_log = test_path + f"\\spg_static_multistart_LOG.txt"
+        with open(static_log, "w") as f:
+            json.dump(static_dict, f)
 
     # -----------------------------------------------------------------------------------
     # Multi-start --Disable  Standard
     # -----------------------------------------------------------------------------------
     else:
         out, elapsed_time, fo = data
-        x_spg, backtracking, iterations, fun_o, gtd, norm_d, log_spg = out
+        x_spg, backtracking, iterations, fun_o, gtd, norm_d = out
         # Statistics:
         rmsd_f = rmsd(x_spg, solution)
         mde_f = mde(x_spg, u, v, lb, ub)
-        print(":: spg results --multi start: False")
-        print(
-            ":: Iter -- bck -- RMSDf ----- MDEf ----- i_val ----- f_val ----- gtd ----- |d| ----- time(s)"
+        logger.info(":: spg results --multi start: False")
+        logger.info(
+            ":: Iter - bck -- RMSDf ----- MDEf"
+            " ----- i_val ----- f_val ----- gtd ----- |d| ----- time(s)"
         )
         prompt_string = (
-            f"   {iterations:<7} {backtracking:<6} {rmsd_f:<11.2e} {mde_f:<10.2e} {fo / 2:<11.2e} "
+            f"   {iterations:<5}: {backtracking:<6} {rmsd_f:<11.2e} {mde_f:<10.2e} {fo / 2:<11.2e} "
             f"{fun_o / 2:<10.2e} {gtd:<10.2e} {norm_d:<10.2e} {elapsed_time:.3f}"
         )
-        print(prompt_string)
-        print(
-            "##################################################################################"
+
+        logger.info(prompt_string)
+        logger.info(
+            "############################################################################################"
         )
 
         # -----------------------------------------------------------------------------
         # Generating output file with statistics:
         # -----------------------------------------------------------------------------
-        now = datetime.now()
-        dt_time = now.strftime("%d-%m-%Y--%H-%M-%S")
-        out_file_log = local_dir + f"\\Teste {filename}\\LOG_({dt_time}).txt"
-        with open(out_file_log, "w") as f:
-            for item in log:
-                if type(item) == list:
-                    for item_spg in item:
-                        f.write(f"{item_spg}\n")
-                    continue
-                f.write(f"{item}\n")
+        static_log = test_path + f"\\spg_static_standard_LOG.txt"
 
-            f.write(
-                "#####################################  INFO  #####################################\n"
-            )
-            f.write(
-                f":: Protein: {filename}, Initial atoms number: {num_atom_init}, "
-                f"after re-ordination {total_atoms_ord}.\n"
-            )
-            f.write(f":: Assessed distances: {m} and known distances: {prop_dist}.\n")
-            if convex:
-                f.write(
-                    f":: Initial objective value for the relaxed problem: {fo_non_scaled:.3e}\n"
-                )
-                f.write(
-                    f":: Initial objective value for the relaxed problem --scaled {fo_scaled:.3e}\n"
-                )
-            else:
-                f.write(f":: Initial objective value: {fo:.3e}\n")
-            f.write(f":: RMSDi = {rmsd_i:<24.2e} MDEi = {mde_i:.2e}\n")
-            f.write(":: spg results --multi start: False\n")
-            # statistics
-            f.write(
-                ":: Iter -- bck -- RMSDf ----- MDEf ----- i_val ----- f_val ----- gtd ----- |d| ----- time(s)\n"
-            )
-            f.write(f"{prompt_string}\n")
-            f.write(
-                "##################################################################################\n"
-            )
+        static_dict = {"node": f'{filename}', "init_atom_#": f"{num_atom_init}",
+                       "atom_#_re-ordination": f'{total_atoms_ord}',
+                       "assessed_dist": f'{m}', "Know_dist": f'{prop_dist}'}
+        if convex:
+            static_dict["convex"] = True
+            static_dict["init_fun_val_relax"] = f'{fo_non_scaled:.4e}'
+            static_dict["init_fun_val_relax_k"] = f'{fo_scaled:.4e}'
+        else:
+            static_dict["convex"] = False
+            static_dict["init_fun_val_relax"] = 'N/A'
+            static_dict["init_fun_val_relax_k"] = 'N/A'
+
+        static_dict["RMSDi"] = f'{rmsd_i:<24.2e}'
+        static_dict["MDEi"] = f'{mde_i:.2e}'
+        static_dict["multi-start"] = False
+        static_dict["standard"] = {"iter": f'{iterations:<7}', "back": f'{backtracking:<6}',
+                                   "RMDSf": f'{rmsd_f:<11.2e}',
+                                   "MDEf": f'{mde_f:<10.2e}', "fun_i": f'{fo / 2:<11.2e}',
+                                   "fun_f": f'{fun_o / 2:<10.2e}',
+                                   "gtd": f'{gtd:<10.2e}', "norm_d": f'{norm_d:<10.2e}',
+                                   "time": f'{elapsed_time:.3f}'}
+
+        with open(static_log, "w") as file:
+            json.dump(static_dict, file)
